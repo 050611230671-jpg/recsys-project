384c3457b73c3c059a446472cfffca99f30b2e5c
050611230671-jpg <050611230671@st.buh.edu.vn>
2026-04-13 12:36:10 +0700

    Fix structure and add real HTML

--- main.py
+++ main.py
@@ -3,13 +3,10 @@ from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
-
-# Đảm bảo thư mục 'templates' tồn tại trên máy bạn
 templates = Jinja2Templates(directory="templates")
 
 @app.get("/", response_class=HTMLResponse)
 async def home(request: Request):
-    # Dữ liệu sản phẩm có thêm link ảnh (img)
     products = [
         {"name": "Giày Sneaker Nike", "price": "1.250.000đ", "img": "https://images.unsplash.com/photo-1542291026-7eec264c27ff?w=500"},
         {"name": "Áo Hoodie Local Brand", "price": "450.000đ", "img": "https://images.unsplash.com/photo-1556821840-3a63f95609a7?w=500"}
