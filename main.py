003ba56d7cfba8c4fb1ade03c26da96d87ae5586
050611230671-jpg <050611230671@st.buh.edu.vn>
2026-04-13 12:28:22 +0700

    Hien thi giao dien dep

--- main.py
+++ main.py
@@ -1,19 +1,14 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
+templates = Jinja2Templates(directory="templates")
 
-@app.get("/")
-async def home():
-    # Bản này không dùng giao diện HTML để tránh lỗi "Internal Server Error"
-    # Nếu web hiện ra dòng chữ dưới đây là bạn đã thành công 100%
-    return {
-        "status": "Success",
-        "message": "Chuc mung! Server cua ban da chay ruc ro tren Google Cloud",
-        "author": "Recsys-app",
-        "products": [
-            {"id": 1, "name": "Giay Sneaker", "price": "1.200k"},
-            {"id": 2, "name": "Ao Hoodie", "price": "450k"}
-        ]
-    }
-
-# Luu y: Khong can them doan if __name__ == "__main__" o day nua
+@app.get("/", response_class=HTMLResponse)
+async def home(request: Request):
+    products = [
+        {"name": "Giày Sneaker", "price": "1.200k", "img": "https://images.unsplash.com/photo-1542291026-7eec264c27ff?w=400"},
+        {"name": "Áo Hoodie", "price": "450k", "img": "https://images.unsplash.com/photo-1556821840-3a63f95609a7?w=400"}
+    ]
+    return templates.TemplateResponse("index.html", {"request": request, "products": products})
